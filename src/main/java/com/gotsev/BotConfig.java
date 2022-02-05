c3f64bef3eff87af6528d32f3e8b6d171395ee4d
sgotsev <sgotsev@cinimex.ru>
2022-02-05 17:30:33 +0300

    remove token and comments

--- src/main/java/com/gotsev/BotConfig.java
+++ src/main/java/com/gotsev/BotConfig.java
@@ -1,11 +1,6 @@
 package com.gotsev;
 
-/**
- * @author Ruben Bermudez
- * @version 1.0
- * @brief Bots configurations
- * @date 20 of June of 2015
- */
+
 public class BotConfig {
 
     public static final String TEST_TOKEN = "token";
